197462e19f85fe82c20901647f9d92e2401b41ff
Hubert Zub <hubert.zub@eyedea.io>
2020-03-26 19:59:36 +0100

    Updates

---
+++ precache-manifest.f720b1ac2ea922cc7d10dce0e0599866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e4964560101da78a4e1d20f35db65e",
+    "url": "/infection-simulator/index.html"
+  },
+  {
+    "revision": "8180ac79ac2d796338c7",
+    "url": "/infection-simulator/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "6b8f472a19316ce70de8",
+    "url": "/infection-simulator/static/css/main.7b903f43.chunk.css"
+  },
+  {
+    "revision": "8180ac79ac2d796338c7",
+    "url": "/infection-simulator/static/js/2.16458e35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/infection-simulator/static/js/2.16458e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8f472a19316ce70de8",
+    "url": "/infection-simulator/static/js/main.0a01d3c7.chunk.js"
+  },
+  {
+    "revision": "96210a74225cdc895d60",
+    "url": "/infection-simulator/static/js/runtime-main.49a5a898.js"
+  }
+]);
